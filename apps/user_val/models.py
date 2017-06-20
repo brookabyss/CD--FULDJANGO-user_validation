d6d1e0973cca7efee467ba246ea3e2cc72574b07
Brook Kebede <brookabyss@gmail.com>
2017-06-20 14:01:49 -0700

    The model checks for username length and I was able to redirect to the show page

--- apps/user_val/models.py
+++ apps/user_val/models.py
@@ -2,4 +2,17 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class UserManager(models.Manager):
+    def check_length(self,postData):
+        if len(postData)<8 or len(postData)>26:
+            return False
+        return True
+    
+
+class Users(models.Model):
+    username=models.CharField(max_length=255)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+    objects=UserManager()
+    def __str__(self):
+        return self.username
--- apps/user_val/views.py
+++ apps/user_val/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render, redirect
-
+from .models import Users
 # Create your views here.
 def index(request):
     return render(request,'user_val/index.html')
+
+def register(request):
+    if request.method=='POST':
+        username=request.POST['user_name']
+        if Users.objects.check_length(username):
+            Users.objects.create(username=username)
+
+            users=Users.objects.all()
+            context={
+                'username': username,
+                'users':users
+            }
+            return render(request,'user_val/show.html', context)
+        else:
+            context={
+                'error': 'Username is not valid!'
+            }
+            return render(request,'user_val/index.html', context)
+
+    return redirect('/')
